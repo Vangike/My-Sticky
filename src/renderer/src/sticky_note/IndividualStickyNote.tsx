9dcc46f50e973b178df86b92959020060621d6e9
Vangike <vangmmike@gmail.com>
2024-08-09 10:43:20 -0500

    Individual sticky note input title

--- src/renderer/src/sticky_note/IndividualStickyNote.tsx
+++ src/renderer/src/sticky_note/IndividualStickyNote.tsx
@@ -1,16 +1,26 @@
 import { StickyNoteInfo } from '@shared/models'
-import { ComponentProps } from 'react'
+import { ComponentProps, useCallback, useState } from 'react'
 
 type StickyNoteHeaderProp = ComponentProps<'div'> & {
   stickyNoteInfo: StickyNoteInfo
 }
 
 export const StickyNoteHeader = ({ stickyNoteInfo, className }: StickyNoteHeaderProp) => {
-  const processedTitle = stickyNoteInfo.title.replace(/^.*[\\/]/, '')
+  const [title, setTitle] = useState(stickyNoteInfo.title.replace(/^.*[\\/]/, ''))
+
+  const handleTitleUpdate = useCallback((e) => {
+    setTitle(e.target.value)
+    console.log(title)
+  }, [])
 
   return (
     <div className={className}>
-      <h1 className="text-2xl">{processedTitle}</h1>
+      <input
+        className="text-2xl w-full bg-inherit"
+        type="text"
+        value={title}
+        onChange={handleTitleUpdate}
+      />
       <h1>{stickyNoteInfo.subtitle}</h1>
     </div>
   )
