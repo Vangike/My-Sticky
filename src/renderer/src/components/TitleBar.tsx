d777022558e628301ad31157a5795cb202a0f783
Vangike <vangmmike@gmail.com>
2024-08-19 20:21:08 -0500

    Title bar props

--- src/renderer/src/components/TitleBar.tsx
+++ src/renderer/src/components/TitleBar.tsx
@@ -1,16 +1,37 @@
-import { ComponentProps } from 'react'
+import { cn } from '@renderer/utils'
+import { ComponentProps, HTMLProps } from 'react'
 
-const TitleButton = ({ value, ...props }: ComponentProps<'button'>) => {
-  return <button className="titlebar-button flex-1 text-base">{value}</button>
+type TitleButtonProps = HTMLProps<HTMLButtonElement> & {
+  withRed?: boolean
+}
+
+const TitleButton = ({
+  className,
+  value,
+  withRed = false,
+  onClick,
+  ...props
+}: TitleButtonProps) => {
+  const titleClass = cn(
+    className,
+    'titlebar-button flex-1 text-base transition ease-in-out duration-100',
+    withRed ? 'hover:bg-red-500' : 'hover:bg-neutral-500'
+  )
+
+  return (
+    <button className={titleClass} onClick={onClick}>
+      {value}
+    </button>
+  )
 }
 
 export const TitleBar = () => {
   return (
-    <nav className="titlebar flex flex-row width-full items-center bg-gray-900 text-white justify-between">
-      <div>Test</div>
+    <nav className="titlebar flex flex-row width-full items-center bg-neutral-800 text-white justify-between">
+      <div></div>
       <div className="flex flex-row w-12 space-x-1">
         <TitleButton value="&minus;" />
-        <TitleButton value="&#10006;" />
+        <TitleButton withRed={true} value="&#10006;" />
       </div>
     </nav>
   )
